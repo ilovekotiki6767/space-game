e3fe9afbbb3830f00cebc804932a51ef9558a6f1
ilovekotiki6767 <godraylllll@gmail.com>
2026-04-25 16:11:56 +0300

    zero out the rotation field

--- src/Game_main.c
+++ src/Game_main.c
@@ -10,6 +10,8 @@ void UpdateAndRender(Game_Platform *platform, const float delta_time) {
     State *state = platform->permanent_storage;
 
     if (!state->initialized) {
+        state->rotation = 0.0f;
+
         // this must always be here!
         state->initialized = True;
     }
