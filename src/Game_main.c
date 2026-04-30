92fd893363427e1a05967661af3ebf3f90051dda
ilovekotiki6767 <godraylllll@gmail.com>
2026-04-30 07:28:59 +0300

    remove mouse smoothing

--- src/Game_main.c
+++ src/Game_main.c
@@ -62,7 +62,6 @@ typedef struct {
     Vec3 velocity;
 
     float pitch, yaw;
-    float target_pitch, target_yaw;
     Bool grounded;
 
     Entity entities[256];
@@ -95,8 +94,6 @@ void UpdateAndRender(Game_Platform *platform) {
         state->velocity = Vector3(0, 0, 0);
         state->yaw = 0.0f;
         state->pitch = 0.0f;
-        state->target_yaw = 0.0f;
-        state->target_pitch = 0.0f;
         state->grounded = False;
 
         Entity *floor = AddEntity(state);
@@ -124,28 +121,17 @@ void UpdateAndRender(Game_Platform *platform) {
     if (!platform->mouse_locked) {
         if (WasPressed(platform->input[GAME_KEY_MOUSE_LEFT])) {
             platform->mouse_locked = True;
-
-            state->target_yaw = state->yaw;
-            state->target_pitch = state->pitch;
         }
     } else {
         if (WasPressed(platform->input[GAME_KEY_ESCAPE])) {
             platform->mouse_locked = False;
         }
 
-        state->target_yaw += platform->mouse_delta_x * 0.0015f;
-        state->target_pitch -= platform->mouse_delta_y * 0.0015f;
+        state->yaw += platform->mouse_delta_x * 0.0015f;
+        state->pitch -= platform->mouse_delta_y * 0.0015f;
 
-        state->target_pitch = Clamp(state->target_pitch, -(PI / 2.0f - 0.1f), PI / 2.0f - 0.1f);
+        state->pitch = Clamp(state->pitch, -(PI / 2.0f - 0.1f), PI / 2.0f - 0.1f);
     }
-
-    float t = 20.0f * platform->delta_time;
-    if (t > 1.0f) {
-        t = 1.0f;
-    }
-
-    state->yaw = Lerp(state->yaw, state->target_yaw, t);
-    state->pitch = Lerp(state->pitch, state->target_pitch, t);
 
     switch (state->mode) {
         case MODE_MENU: {
